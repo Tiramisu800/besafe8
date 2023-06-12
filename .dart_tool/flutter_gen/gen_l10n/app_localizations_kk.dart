373540e09f6031202dc9311fd1f752982af999d4
Tiramisu800 <zhamal.baizen@gmail.com>
2023-06-12 20:47:22 +0600

    App icon and name

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_kk.dart
@@ -0,0 +1,132 @@
+import 'app_localizations.dart';
+
+/// The translations for Kazakh (`kk`).
+class AppLocalizationsKk extends AppLocalizations {
+  AppLocalizationsKk([String locale = 'kk']) : super(locale);
+
+  @override
+  String get language => 'Қазақ тілі';
+
+  @override
+  String get helloworld => 'Салам әлем';
+
+  @override
+  String get titleprofile => 'Кеттік!';
+
+  @override
+  String get subtitleprofile => 'Профиль деректерін толтырыңыз';
+
+  @override
+  String get name => 'Атыңыз';
+
+  @override
+  String get nameval => 'Өтінеміз, атыңызды дұрыс жазыңыз!';
+
+  @override
+  String get nameisreg => 'Атыңыз міндетті!';
+
+  @override
+  String get email => 'Электрондық пошта';
+
+  @override
+  String get emailisreg => 'Электрондық пошта қажет!';
+
+  @override
+  String get emailvalid => 'Жарамды электрондық пошта енгізіңіз.';
+
+  @override
+  String get submit => 'Растау';
+
+  @override
+  String get plsaddim => 'Суретіңізді қосыңыз';
+
+  @override
+  String get helloword => 'Қайырлы таң, Be safe!';
+
+  @override
+  String get wherego => 'Қайда барармыз?';
+
+  @override
+  String get destination => 'Барар жер:';
+
+  @override
+  String get searchfordest => 'Баратын жерді іздеу';
+
+  @override
+  String get from => 'Қайдан:';
+
+  @override
+  String get pah => 'Төлем тарихы';
+
+  @override
+  String get rih => 'Саяхат тарихы';
+
+  @override
+  String get invite => 'Достарды шақыру';
+
+  @override
+  String get promo => 'Промокодтар';
+
+  @override
+  String get settings => 'Параметрлер';
+
+  @override
+  String get support => 'Қолдау';
+
+  @override
+  String get logout => 'Кету';
+
+  @override
+  String get mptitle => 'Профиль';
+
+  @override
+  String get home => 'Үй мекенжайы';
+
+  @override
+  String get homeisreg => 'Үй мекенжайы міндетті!';
+
+  @override
+  String get businnesadd => 'Жұмыс мекенжайы';
+
+  @override
+  String get businnesaddisreg => 'Жұмыс мекенжайы қажет!';
+
+  @override
+  String get shop => 'Сауда орталығы';
+
+  @override
+  String get shopisreg => 'Сауда орталығы қажет!';
+
+  @override
+  String get update => 'Жаңарту';
+
+  @override
+  String get logasd => 'Жүргізуші ретінде кіру';
+
+  @override
+  String get logasu => 'Пассажир ретінде кіру';
+
+  @override
+  String get english => 'Ағылшын';
+
+  @override
+  String get russian => 'Орыс';
+
+  @override
+  String get kazakh => 'Қазақ';
+
+  @override
+  String get back => 'Артқа';
+
+  @override
+  String get theme => 'Тақырыптар';
+
+  @override
+  String get light => 'Жарық';
+
+  @override
+  String get dark => 'Қараңғы';
+
+  @override
+  String get custom => 'Арнаулы';
+}
